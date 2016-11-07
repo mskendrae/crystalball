b7a6c6a8fc7a9fdecdfa5468865acb4747b966fa
Kendra Egert <Kendra Egert>
2016-11-06 19:00:13 -0500

    initial commit

---
+++ CyrstalBallApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using CyrstalBallApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CyrstalBallApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult ShakeBall()
+        {
+
+            RandomStrings rndStr = new RandomStrings();
+             
+                
+            return Json(rndStr.grabRandomString(), JsonRequestBehavior.AllowGet);  
+         }
+    }
+}
---
+++ CyrstalBallApp/Models/crystallball.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CyrstalBallApp.Models
+{
+    public class RandomStrings
+    {
+        private string[] availableStrings = { "Code is poetry", "Ladies prefer coding", "We Can Code IT", "Puppies are cute" };
+        private Random rnd = new Random(); //just grabbing a random object so we can use it later
+
+        //get string
+        internal string grabRandomString()
+        {
+            return availableStrings[rnd.Next(0, availableStrings.Length)];
+        }
+
+
+    }
+
+    }
